0a6e517487d63be1f261870603c9b6d3958d8bde
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-02-03 22:32:59 +0100

    updating page title

--- js/blogdetails.js
+++ js/blogdetails.js
@@ -1,5 +1,32 @@
-import { fetchPostById } from "./api.js";
+import { fetchAllPosts, fetchPostById } from "./api.js";
 import { displayPostDetails } from "./render-blogdetails.js";
+
+export async function updatePageTitle(postId) {
+  try {
+    const posts = await fetchAllPosts();
+    console.log("All Posts:", posts);
+    const post = posts.find((post) => post.id === parseInt(postId));
+
+    if (post && post.title) {
+      document.title = post.title;
+    } else {
+      throw new Error("Post data or Title is undefined");
+    }
+  } catch (error) {
+    document.title = "ERROR: can not fetch title";
+    console.error("Error: can not fetch title", error);
+  }
+}
+
+document.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("post-link")) {
+    event.preventDefault();
+
+    const postId = new URL(event.target.href).searchParams.get("id");
+
+    await updatePageTitle(postId);
+  }
+});
 
 export async function postDetailPage() {
   const postDetailsContainer = document.querySelector(".post-details");
@@ -8,6 +35,7 @@ export async function postDetailPage() {
     const url = new URL(location.href);
     console.log("Current URL:", url.href);
     const postId = url.searchParams.get("id");
+    await updatePageTitle(postId);
     console.log("Retrieved Post ID:", postId);
     if (postId) {
       const post = await fetchPostById(postId);
