53b77d5ffa0344efcf350b6d7f2ebbbc31b496cc
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-02-04 13:53:17 +0100

    styling blog details

--- js/render-blogdetails.js
+++ js/render-blogdetails.js
@@ -1,22 +1,29 @@
 export function displayPostDetails(post) {
   const postDetailsContainer = document.querySelector(".post-details");
+  postDetailsContainer.classList.add("m-top", "m-bottom");
 
-  const postElement = document.createElement("div");
+  const postContentContainer = document.createElement("div");
+  postContentContainer.classList.add("frame");
 
   const postTitle = document.createElement("h2");
   postTitle.textContent = post.title || "Untitled";
 
+  const postElement = document.createElement("div");
+  postElement.classList.add("img-details-container");
   if (post.img) {
     const imgElement = document.createElement("img");
     imgElement.src = post.img;
-    imgElement.classList.add("img-postlist");
-    postElement.append(imgElement);
+    imgElement.classList.add("img-details", "img-details-position");
+    postElement.appendChild(imgElement);
   }
+
   const contentElement = document.createElement("div");
   contentElement.innerHTML = post.content;
 
   postDetailsContainer.innerHTML = "";
-  postDetailsContainer.appendChild(postTitle);
-  postDetailsContainer.appendChild(contentElement);
-  postDetailsContainer.appendChild(postElement);
+  postDetailsContainer.appendChild(postContentContainer);
+  postContentContainer.appendChild(postTitle);
+  postContentContainer.appendChild(postElement);
+
+  postContentContainer.appendChild(contentElement);
 }
